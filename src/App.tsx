4aa49a4b182338b9900ccd140fca56699187a219
Dklim2005 <dklim2005@yandex.ru>
2026-01-09 09:29:28 +0000

    feat: setup app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Navigate } from "react-router-dom"
+import Header from "./components/Header/Header"
+import BoardPage from "./pages/BoardPage/BoardPage"
+import CreateTaskPage from "./pages/CreateTaskPage/CreateTaskPage"
+import TaskPage from "./pages/TaskPage/TaskPage"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Navigate to="/board" replace />} />
+        <Route path="/board" element={<BoardPage />} />
+        <Route path="/create" element={<CreateTaskPage />} />
+        <Route path="/task/:id" element={<TaskPage />} />
+      </Routes>
     </>
   )
 }
